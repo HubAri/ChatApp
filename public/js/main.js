4aac915d29126d5e41cd79ef4231fcf0e311719f
Arian <moharramzadeh@cip.ifi.lmu.de>
2023-11-01 16:17:17 +0100

    added time of message and chatAdmin

--- public/js/main.js
+++ public/js/main.js
@@ -22,12 +22,18 @@ chatForm.addEventListener('submit', (e)=> {
     
     //Emit message to server
     socket.emit('chatMessage', msg);
+
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 })
 
 // Output message to DOM
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = ` ${message} </p>`;
+    div.innerHTML = ` <p class = "meta"> ${message.username} <span> ${message.time}</span> </p>
+    ${message.text}
+    </p>`;
     document.querySelector('.chat-messages').appendChild(div);
 }
