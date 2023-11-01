e508069f0b5c1c1bebae86cb58e09800ebaeb425
Arian <moharramzadeh@cip.ifi.lmu.de>
2023-11-01 19:56:07 +0100

    list currently joined users

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ io.on('connection', socket => {
 
     //Broadcast when a user connects
     socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
-    })
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+        room : user.room,
+        users: getRoomUsers(user.room)
+    });
+    });
 
    // Listen for chatMessage
    socket.on('chatMessage', msg => {
@@ -41,8 +47,15 @@ io.on('connection', socket => {
 
         if(user){
             io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+             // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+        room : user.room,
+        users: getRoomUsers(user.room)
+    });
         }
 
+   
        }); 
     
 });
