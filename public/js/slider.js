4163d4d4ec5f9462fce75eac33b36824a8c13a45
Arian <moharramzadeh@cip.ifi.lmu.de>
2023-11-04 17:31:34 +0100

    adjusted left and right movement of bar

--- public/js/slider.js
+++ public/js/slider.js
@@ -10,19 +10,23 @@ left.addEventListener("click", () => {
   if (slideNumber < images.length) {
     slider.style.transform = `translateX(-${slideNumber * 800}px)`;
     slideNumber++;
+    changeColor(buttons, slideNumber);
   } else {
     slider.style.transform = `translateX(0px)`;
     slideNumber = 1;
+    changeColor(buttons, slideNumber);
   }
 });
 
 right.addEventListener("click", () => {
   if (slideNumber > 1) {
-    slider.style.transform = `translateX(-${800}px)`;
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
     slideNumber--;
+    changeColor(buttons, slideNumber);
   } else {
     slider.style.transform = `translateX(0px)`;
     slideNumber = 1;
+    
   }
 });
 
@@ -31,3 +35,30 @@ for (let i= 0; i<images.length; i++){
   div.className = "buttonRow";
   footer.appendChild(div)
 }
+
+const buttons = document.querySelectorAll("buttonRow")
+
+buttons[0].style.backgroundColor ="black" 
+
+function changeColor(buttons, slideNumber){
+resetBG();
+buttons[slideNumber-1].style.backgroundColor = "black"
+console.log(`Changed button ${slideNumber-1}`)
+}
+
+const resetBG = () => {
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "transparent";
+  })
+}
+
+
+
+buttons.forEach((button, i)=>{
+  button.addEventListener("click", () =>{
+    resetBG();
+    slider.style.transform = `translateX(-${i*800}px)`;
+    slideNumber = i + 1;
+    buttons[i].style.backgroundColor = "black"
+  })
+})
